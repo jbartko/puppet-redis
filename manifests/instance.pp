2cb31c4880b2e08c247ee096074ec6de9c01d137
John Bartko <jbartko@txstate.edu>
2013-08-11 21:02:02 -0500

    cleanup

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,16 +1,17 @@
 #
 define redis::instance (
   $port     = '6379',
-  $conf     = "${redis::dir_conf}/${port}.conf",
-  $conf_dir = $redis::dir_conf,
-  $lib_dir  = $redis::dir_lib,
-  $db_file  = "${redis::dir_lib}/${port}.rdb",
-  $log_file = "${redis::dir_log}/${port}.log",
-  $pid_file = "${redis::dir_run}/${port}.pid",
 ) {
+  $conf_dir = $redis::dir_conf
+  $conf     = "${conf_dir}/${port}.conf"
+  $lib_dir  = $redis::dir_lib
+  $db_file  = "${lib_dir}/${port}.rdb"
+  $log_file = "${redis::dir_log}/${port}.log"
+  $pid_file = "${redis::dir_run}/${port}.pid"
+
   if ! defined(Class['redis']) { fail('You must include Class[\'redis\']!') }
 
-  file { "/etc/redis.d/${port}.conf":
+  file { "${conf_dir}/${port}.conf":
     ensure  => present,
     content => template('redis/instance.conf.erb'),
   }
--- manifests/instance/params.pp
+++
@@ -1,6 +0,0 @@
-#
-class redis::instance::params {
-  $port = 6379
-}
-
-# vim: set ts=2 sw=2 et ft=puppet:
